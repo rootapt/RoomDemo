cf583d7b43baeadca8389c530f1e255800a05ca9
МельниковДМ <mielnikov.d@bk.ru>
2026-03-09 10:44:03 +0700

    Завершение работы функции MainScreen

--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -5,6 +5,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
@@ -12,25 +27,6 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomdemo.ui.theme.RoomDemoTheme
-import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,8 +41,8 @@ class MainActivity : ComponentActivity() {
                             it,
                             "MainViewModel",
                             MainViewModelFactory(
-                                LocalContext.current.applicationContext
-                                        as Application)
+                                LocalContext.current.applicationContext as Application
+                            )
                         )
                         ScreenSetup(
                             modifier = Modifier.padding(innerPadding),
@@ -57,71 +53,18 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
-    @Composable
-    fun TitleRow(head1: String, head2: String, head3: String) {
-        Row(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primary)
-                .fillMaxWidth()
-                .padding(5.dp)
-        ) {
-            Text(head1, color = Color.White,
-                modifier = Modifier
-                    .weight(0.1f))
-            Text(head2, color = Color.White,
-                modifier = Modifier
-                    .weight(0.2f))
-            Text(head3, color = Color.White,
-                modifier = Modifier.weight(0.2f))
-        }
-    }
-
-    @Composable
-    fun ProductRow(id: Int, name: String, quantity: Int) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(5.dp)
-        ) {
-            Text(id.toString(), modifier = Modifier
-                .weight(0.1f))
-            Text(name, modifier = Modifier.weight(0.2f))
-            Text(quantity.toString(), modifier = Modifier.weight(0.2f))
-        }
-    }
-
-    @Composable
-    fun CustomTextField(
-        title: String,
-        textState: String,
-        onTextChange: (String) -> Unit,
-        keyboardType: KeyboardType
-    ) {
-        OutlinedTextField(
-            value = textState,
-            onValueChange = { onTextChange(it) },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = keyboardType
-            ),
-            singleLine = true,
-            label = { Text(title)},
-            modifier = Modifier.padding(10.dp),
-            textStyle = TextStyle(
-                fontWeight = FontWeight.Bold,
-                fontSize = 30.sp
-            )
-        )
-    }
 }
+
 @Composable
 fun ScreenSetup(modifier: Modifier = Modifier, viewModel: MainViewModel) {
     val allProducts by viewModel.allProducts.observeAsState(listOf())
     val searchResults by viewModel.searchResults.observeAsState(listOf())
-    MainScreen(modifier = modifier,
+    MainScreen(
+        modifier = modifier,
         allProducts = allProducts,
         searchResults = searchResults,
-        viewModel = viewModel)
+        viewModel = viewModel
+    )
 }
 
 @Composable
@@ -131,6 +74,137 @@ fun MainScreen(
     searchResults: List<Product>,
     viewModel: MainViewModel
 ) {
+    var productName by remember { mutableStateOf("") }
+    var productQuantity by remember { mutableStateOf("") }
+    var searching by remember { mutableStateOf(false) }
+    val onProductTextChange = { text : String ->
+        productName = text
+    }
+    val onQuantityTextChange = { text : String ->
+        productQuantity = text
+    }
+
+    Column(
+        horizontalAlignment = CenterHorizontally,
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        CustomTextField(
+            title = "Product Name",
+            textState = productName,
+            onTextChange = onProductTextChange,
+            keyboardType = KeyboardType.Text
+        )
+        CustomTextField(
+            title = "Quantity",
+            textState = productQuantity,
+            onTextChange = onQuantityTextChange,
+            keyboardType = KeyboardType.Number
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+            Button(onClick = {
+                if (productQuantity.isNotEmpty()) {
+                    viewModel.insertProduct(
+                        Product(
+                            productName,
+                            productQuantity.toInt()
+                        )
+                    )
+                    searching = false
+                }
+            }) {
+                Text("Add")
+            }
+            Button(onClick = {
+                searching = true
+                viewModel.findProduct(productName)
+            }) {
+                Text("Search")
+            }
+            Button(onClick = {
+                searching = false
+                viewModel.deleteProduct(productName)
+            }) {
+                Text("Delete")
+            }
+            Button(onClick = {
+                searching = false
+                productName = ""
+                productQuantity = ""
+            }) {
+                Text("Clear")
+            }
+        }
+    }
+    LazyColumn(
+        Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ) {
+        val list = if (searching) searchResults else allProducts
+        item {
+            TitleRow(head1 = "ID", head2 = "Product", head3 = "Quantity")
+        }
+        items(list) { product ->
+            ProductRow(id = product.id, name = product.productName,
+                quantity = product.quantity)
+        }
+    }
+}
+
+@Composable
+fun TitleRow(head1: String, head2: String, head3: String) {
+    Row(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Text(head1, color = Color.White,
+            modifier = Modifier
+                .weight(0.1f))
+        Text(head2, color = Color.White,
+            modifier = Modifier
+                .weight(0.2f))
+        Text(head3, color = Color.White,
+            modifier = Modifier.weight(0.2f))
+    }
+}
+@Composable
+fun ProductRow(id: Int, name: String, quantity: Int) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Text(id.toString(), modifier = Modifier
+            .weight(0.1f))
+        Text(name, modifier = Modifier.weight(0.2f))
+        Text(quantity.toString(), modifier = Modifier.weight(0.2f))
+    }
+}
+
+@Composable
+fun CustomTextField(
+    title: String,
+    textState: String,
+    onTextChange: (String) -> Unit,
+    keyboardType: KeyboardType
+) {
+    OutlinedTextField(
+        value = textState,
+        onValueChange = { onTextChange(it) },
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+        singleLine = true,
+        label = { Text(title) },
+        modifier = Modifier.padding(10.dp),
+        textStyle = TextStyle(fontWeight = FontWeight.Bold, fontSize = 30.sp)
+    )
 }
 
 class MainViewModelFactory(val application: Application) :
